4d7f36e8549a3ab6bd6de7375e8cbdb1063718c9
mplulu <hgia1234@gmail.com>
2024-04-10 11:39:26 +0700

    update srano gui cho nhanh

--- main.go
+++ main.go
@@ -5,11 +5,12 @@ import (
 	"fmt"
 	"mplulu/srano/renv"
 	"os"
+	"strings"
 
 	"github.com/mplulu/rano"
 )
 
-var message = flag.String("message", "", "message")
+var messageArg = flag.String("message", "", "message")
 
 type ENV struct {
 	Token   string `yaml:"token"`
@@ -25,8 +26,13 @@ func main() {
 	}
 	renv.Parse("", fmt.Sprintf("%v/.srano", homePath), &env)
 
+	message := *messageArg
+	if message == "" {
+		message = strings.Join(os.Args[1:], " ")
+	}
+
 	r := rano.NewRano(env.Token, []string{env.GroupId})
-	err = r.Send(*message)
+	err = r.Send(message)
 	if err != nil {
 		panic(err)
 	}
